7d76fa25bd79b9471e45c5be86d2780fcd907059
santiagoPHDZ <150392238+santiagoPHDZ@users.noreply.github.com>
2023-12-06 12:46:06 -0600

    Update from terminal

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,3 +1,4 @@
+import MainContainer from "@/components/containers/main-container"
 import NavBar from "@/components/navbar"
 import { getUser } from "@/lib/services/user"
 
@@ -6,8 +7,10 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 
     return (
         <main className="w-screen h-screen">
-            <NavBar user={null}/>
-            {children}
+            <NavBar user={null} />
+            <MainContainer>
+                {children}
+            </MainContainer>
         </main>
     )
 }
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,17 @@
+import MainContainer from "@/components/containers/main-container"
 import NavBar from "@/components/navbar"
 import { getUser } from "@/lib/services/user"
 
-const Layout = async ({children}: {children: React.ReactNode}) => {
+const Layout = async ({ children }: { children: React.ReactNode }) => {
 
   const user = await getUser()
 
   return (
-    <main>
-        <NavBar user={user}/>
+    <main className="w-screen h-screen">
+      <NavBar user={user} />
+      <MainContainer>
         {children}
+      </MainContainer>
     </main>
   )
 }
