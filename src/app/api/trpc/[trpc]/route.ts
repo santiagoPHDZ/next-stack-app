4b42d21b28a7b811ed612f35b903adbbc92bc56d
santiagoPHDZ <150392238+santiagoPHDZ@users.noreply.github.com>
2023-12-06 13:27:20 -0600

    Update from terminal

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -2,10 +2,12 @@
 // import { appRouter } from '../../../../trpc';
 // import { createTRPCContext } from '../../../../trpc/trpc';
 // import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-// import { NextRequest } from 'next/server';
+import { NextRequest } from 'next/server';
 
 // // Handle TRPC endpoint requests
-// const handler = (req: NextRequest) =>
+const handler = (req: NextRequest) => {
+
+}
 //     fetchRequestHandler({
 //         endpoint: '/api/trpc',
 //         req,
@@ -17,4 +19,4 @@
 //         }
 //     });
 
-// export { handler as GET, handler as POST };
+export { handler as GET, handler as POST };
