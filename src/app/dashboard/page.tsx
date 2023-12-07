82b5df43376d1eb0144611e687bc000841f36f2d
santiagoPHDZ <150392238+santiagoPHDZ@users.noreply.github.com>
2023-12-07 11:49:38 -0600

    Update from terminal

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,9 @@
 "use client"
-// import EditUserForm from "@/components/forms/edit-user-form";
+
+import EditUserForm from "@/components/forms/edit-user-form";
 import Indicator from "@/components/indicator";
 import { Button } from "@/components/ui/button";
-// import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getUser } from "@/server/services/user";
 import { apiClient } from "@/trpc/trpc-provider";
 
@@ -11,24 +12,30 @@ const Page = () => {
 
     // if (!user) return <Indicator status="unauth" />
 
+    const { data: user } = apiClient.user.getCurrent.useQuery()
+
     const { mutate, data, error, status } = apiClient.user.postCurrent.useMutation()
     return (
         <div className="w-full h-full">
-            {/* <Card className="w-full mt-8">
+            <Card className="w-full mt-8">
                 <CardHeader>
                     <CardTitle>
                         Edit user
                     </CardTitle>
                 </CardHeader>
                 <CardContent>
-                    <EditUserForm user={user} />
+                    {
+                        user ? (
+                            <EditUserForm user={user} />
+                        ) : null
+                    }
                 </CardContent>
-            </Card> */}
+            </Card>
             hi
             <Button onClick={() => {
                 mutate()
             }}>
-                
+
                 click
             </Button>
 
