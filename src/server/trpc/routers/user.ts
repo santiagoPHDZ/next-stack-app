e91fceb09128ff5f1771e72eb66c474efa968d53
santiagoPHDZ <150392238+santiagoPHDZ@users.noreply.github.com>
2023-12-07 12:12:10 -0600

    Update from terminal

--- src/server/trpc/routers/user.ts
+++ src/server/trpc/routers/user.ts
@@ -4,9 +4,14 @@ import { db } from "@/lib/prisma";
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 import { getUser } from "@/server/services/user";
 import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 
 export const userRouter = createTRPCRouter({
-    update: protectedProcedure.input(updateUserTRPCSchema).mutation(async ({ input, ctx }) => {
+    update: protectedProcedure.input(z.object({
+        firstName: z.string().max(25).min(1).optional(),
+        lastName: z.string().max(25).min(1).optional(),
+        imageUrl: z.string().optional(),
+    })).mutation(async ({ input, ctx }) => {
 
         // const { userId } = ctx
         // const { firstName, lastName, imageUrl } = input
